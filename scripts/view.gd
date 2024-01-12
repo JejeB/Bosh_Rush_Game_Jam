eea5c1695752c3aa2ce50d6e012f0eac2247409d
jeje <jeje@bonnefoi.com>
2024-01-12 21:49:21 +0100

    top down camera

--- scripts/view.gd
+++ scripts/view.gd
@@ -3,16 +3,11 @@ extends Node3D
 @export_group("Properties")
 @export var target: Node
 
-@export_group("Zoom")
-@export var zoom_minimum = 16
-@export var zoom_maximum = 4
-@export var zoom_speed = 10
-
-@export_group("Rotation")
-@export var rotation_speed = 120
+@export_group("Placement")
+@export var distance = 10
+@export var height = 10
 
 var camera_rotation:Vector3
-var zoom = 10
 
 @onready var camera = $Camera
 
@@ -23,31 +18,8 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	
-	# Set position and rotation to targets
-	
-	self.position = self.position.lerp(target.position, delta * 4)
-	rotation_degrees = rotation_degrees.lerp(camera_rotation, delta * 6)
-	
-	camera.position = camera.position.lerp(Vector3(0, 0, zoom), 8 * delta)
-	
-	handle_input(delta)
+	camera.look_at(target.position)
+	self.position = target.position
+	camera.position = Vector3(0, height, distance)
+	pass
 
-# Handle input
-
-func handle_input(delta):
-	
-	# Rotation
-	
-	var input := Vector3.ZERO
-	
-	input.y = Input.get_axis("camera_left", "camera_right")
-	input.x = Input.get_axis("camera_up", "camera_down")
-	
-	camera_rotation += input.limit_length(1.0) * rotation_speed * delta
-	camera_rotation.x = clamp(camera_rotation.x, -80, -10)
-	
-	# Zooming
-	
-	zoom += Input.get_axis("zoom_in", "zoom_out") * zoom_speed * delta
-	zoom = clamp(zoom, zoom_maximum, zoom_minimum)
