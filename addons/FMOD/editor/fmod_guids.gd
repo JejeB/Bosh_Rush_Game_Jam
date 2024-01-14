c0425327f5c8a00c92f770f537efb74dcc319316
Jeff Carpenter <153320043+soundguyjeff@users.noreply.github.com>
2024-01-13 20:55:40 -0600

    FMOD Integration

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,18 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'Boss Rush Game Jam 2023.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const JUMP = "{f887d8f5-c105-4503-86ca-871b7c9bf359}"
+
+class Busses:
+	const MASTER_BUS = "{0d13739b-b545-4ddb-8de5-c990a5732626}"
+	const REVERB = "{1c273448-b4e5-4e84-8778-54b58fe204da}"
+
+class Banks:
+	const MASTER = "{d5552bb2-c55f-4580-b3c1-98923ee6800a}"
+
