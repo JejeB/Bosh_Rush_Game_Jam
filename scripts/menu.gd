f67198c5ed2239766ddce371e9827b843263546d
Dunateo <bruvalentin84@icloud.com>
2024-01-29 23:07:27 -0500

    Menu Ui finished

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,36 +1,29 @@
 extends Control
 
 @onready var main_scene = "res://scenes/main.tscn"
-@onready var mask_texture: TextureRect = $SeparateMask/VBoxContainer/CenterContainer/MaskTexture
+@onready var mask_sprite: Sprite2D = $GridContainer/CenterContainer/MaskSprite
+@onready var animation_player: AnimationPlayer = $GridContainer/CenterContainer/AnimationPlayer
+@onready var mask_timer: Timer = $GridContainer/CenterContainer/MaskClickedTimer
 
-var follower_point
+var click_count :int = 0
 
-func _ready():
-	#calculate pivot point
-	call_deferred("calculate_pivot_offset")
-	
 func _process(delta):
-	#print(get_global_mouse_position())
-	#print(mask_texture.position)
 	
-	var angle_to_mouse = mask_texture.pivot_offset.angle_to(get_global_mouse_position())
-	print(rad_to_deg(angle_to_mouse))
-	#print(mask_texture.global_position)
-	
-	#mask_texture.set_rotation(angle_to_mouse)
-	
+	if Input.is_action_just_pressed("click_gauche"):
+		animation_player.play("menu_animation")
+		mask_timer.start()
+		click_count += 1
+		
+	elif click_count >= 3:
+		print("[Play] Easter egg sound")
+		click_count = 0
+	else :
+		mask_sprite.look_at(get_global_mouse_position())
+
+# go to the main scene
 func _on_start_game_button_down():
 	get_tree().change_scene_to_file(main_scene)
 
-
-func calculate_pivot_offset():
-	var middle_position = mask_texture.size/2
-	middle_position.x = mask_texture.global_position.x + middle_position.x
-	middle_position.y = mask_texture.global_position.y + middle_position.y
-	mask_texture.pivot_offset = middle_position
-	
-	follower_point = mask_texture.size/2
-	follower_point.y = mask_texture.global_position.y + follower_point.y
-	
-	print(mask_texture.pivot_offset)
-	print(follower_point)
+# Animation is finished
+func _on_mask_clicked_timer_timeout():
+	animation_player.stop()
