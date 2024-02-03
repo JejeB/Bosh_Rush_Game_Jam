d04d8454757bbf771e30edfa9a4a05b17d6e6dc0
Dunateo <bruvalentin84@icloud.com>
2024-02-02 19:24:40 -0500

    add camera shake when hitting the boss

--- scripts/view.gd
+++ scripts/view.gd
@@ -11,25 +11,42 @@ extends Node3D
 @export var freeze_time : float = 0.5
 @export var traveling_time : float = 0.2
 
-enum Camera_State {FOLLOW, FREEZE, WAIT, TRANSITION}
+@export_group("Shake Properties")
+@export var trauma_reduction_rate : float = 1.0
+@export var noise : FastNoiseLite
+@export var noise_speed : float =  50.0
+@export var max_x : float = 10.0
+@export var max_y : float = 10.0
+@export var max_z : float = 5.0
+@export var trauma_amount : float = 0.6
+@export var shake_time : float = 0.2
+
+enum Camera_State {FOLLOW, FREEZE, WAIT, TRANSITION, SHAKING}
 
 var camera_rotation:Vector3
 var last_seen_position:Vector3
 var camera_freeze: Camera_State = Camera_State.FOLLOW
 
+var trauma = 0.0
+var time = 0.0
+
 @onready var camera = $Camera
 @onready var camera_freeze_timer: Timer = $Camera/CameraFreezeTimer
 
-
+# Freeze properties
+@onready var initial_rotation: Vector3 = self.rotation_degrees
+@onready var camera_shake_timer: Timer = $Camera/ShakeTimer
 
 
 func _ready():
 	camera_rotation = rotation_degrees # Initial rotation
 	camera_freeze_timer.wait_time = freeze_time
+	camera_shake_timer.wait_time = shake_time
 
 # every frame calculation
 func _physics_process(delta):
 	camera_calculus(self.camera_freeze)
+	camera_shaking(delta, self.camera_freeze)
 
 # all the calculus depending on the situation
 func camera_calculus(camera_freeze):
@@ -73,3 +90,31 @@ func _on_player_swap_positon():
 # Defrooze camera
 func _on_camera_freeze_timer_timeout():
 	self.camera_freeze = Camera_State.TRANSITION
+
+# Shake Camera
+func _on_boss_hp_changed(value):
+	self.camera_freeze = Camera_State.SHAKING
+	add_trauma(trauma_amount)
+	camera_shake_timer.start()
+
+func camera_shaking(delta, camera_freeze):
+	if camera_freeze == Camera_State.SHAKING:
+		time += delta
+		trauma = max(trauma - delta * trauma_reduction_rate, 0.0)
+		
+		self.rotation_degrees.x =  initial_rotation.x + max_x * get_shake_intensity() * get_noise_from_seed(0)
+		self.rotation_degrees.y =  initial_rotation.y + max_y * get_shake_intensity() * get_noise_from_seed(1)
+		self.rotation_degrees.z =  initial_rotation.z + max_z * get_shake_intensity() * get_noise_from_seed(2)
+
+func add_trauma(trauma_amount : float):
+	trauma = clamp(trauma + trauma_amount, 0.0, 1.0)
+
+func get_shake_intensity() -> float:
+	return trauma * trauma
+
+func get_noise_from_seed(_seed : int) -> float:
+	noise.seed = _seed
+	return noise.get_noise_1d(time * noise_speed)
+
+func _on_shake_timer_timeout():
+	self.camera_freeze = Camera_State.FOLLOW
