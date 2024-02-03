95c4b6c3184da4f6c2bfaef3591ccc1b54d374ca
Dunateo <bruvalentin84@icloud.com>
2024-02-03 15:18:28 -0500

    Add Bush cut tutorial

--- scripts/BushCuttingTutorial.gd
+++ scripts/BushCuttingTutorial.gd
@@ -1,7 +1,14 @@
 extends Node3D
 
+@export var dialog:Array = ["PRESS <Right Click> with the <MOUSE> to use your SWORD"]
+
 @onready var bush_object: StaticBody3D = $BushObject
 @onready var activation_area: Area3D = $ActivationArea
+@onready var dialog_hud = $"../DialogHud"
+@onready var dialog_label = $"../DialogHud/CenterContainer/DialogLabel"
+@onready var dialog_center = $"../DialogHud/CenterContainer"
+
+var player_is_in_area = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,13 +16,19 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if Input.is_action_just_pressed("click_gauche") and player_is_in_area:
+		dialog_center.hide()
 
 
 func _on_activation_area_body_entered(body):
-	print(body.get_name())
+	if body.get_name() == 'Player' and !player_is_in_area:
+		player_is_in_area = true
+		dialog_label.text = dialog[0]
+		dialog_center.show()
+		dialog_hud.show()
 
 
-func _on_activation_area_area_entered(area):
-	print(area.get_name())
+func _on_activation_area_area_entered(area: Area3D):
+	if area.is_in_group('player_sword'):
+		bush_object.queue_free()
