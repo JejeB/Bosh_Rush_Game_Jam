a0e450293425a80dacc8b413ca28fbdb7edefcb9
jeje <jeje@bonnefoi.com>
2024-02-04 23:42:20 +0100

    fix bug intro

--- scripts/CinemaCamera.gd
+++ scripts/CinemaCamera.gd
@@ -1,10 +1,9 @@
 extends Node3D
 
 @onready var animation : AnimationPlayer = $CameraAnimation
-@onready var boss: CharacterBody3D = $"../Boss"
+@onready var boss = $"../boss1/AnimationPlayer"
 @onready var control_node: Control = $"../Control"
 @onready var main_scene = "res://scenes/main.tscn"
-@onready var dragon_area: Node3D = $"../dragon7"
 
 enum State {PLAY, STOP, TRANSITION}
 
@@ -12,8 +11,7 @@ var cinema_state = State.PLAY
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	dragon_area.visible = false
-	boss.jump_path.visible = false
+	boss.play("boss idle1")
 	animation.play("camera_cut_scene")
 
 
@@ -26,11 +24,10 @@ func add_boss_name():
 	control_node.show()
 
 func boss_display_hide_attack_zone():
-	boss.zone_attack_cut_scene = false
+	pass
 	
 func begin_combat():
 	get_tree().change_scene_to_file(main_scene)
 
 func display_area_effect():
-	boss.visible = false
-	dragon_area.visible = true
+	pass
